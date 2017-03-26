e22f75101171350e2b3ebce0fa5fe2ca6381adb7
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-03-27 01:51:38 +0530

    Working AutoScroll is added

---
+++ apps/AutoScroll/Code/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.root.autoscroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+{
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setSelected(true);
+        textView.setSingleLine(true);
+    }
+}
