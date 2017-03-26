14ac3e39dc95914ce973264c7de2da0e505ba17d
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-03-26 16:30:57 +0530

    Working Adds CustomAPpBarCumStatusBar

--- apps/CustomAppBarCumStatusBar/Code/MainActivity.java
+++ apps/CustomAppBarCumStatusBar/Code/MainActivity.java
@@ -9,11 +9,14 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     int violet;
+    int darkViolet;
     ActionBar actionBar;
     ImageView logo;
 
@@ -24,22 +27,39 @@ public class MainActivity extends AppCompatActivity {
 
         initializeClassObjects();
 
-        actionBar.setTitle("Main Title");
-        actionBar.setSubtitle("Subtitle");
-
-        setBackgroundColor();
-        setLogo();
+        customizeAppBar();
+        customizeStatusBar();
     }
 
     private void initializeClassObjects()
     {
         violet = ContextCompat.getColor(MainActivity.this, R.color.violet);
+        darkViolet = ContextCompat.getColor(MainActivity.this, R.color.darkViolet);
         actionBar = getSupportActionBar();
+    }
+
+    private void customizeAppBar()
+    {
+        actionBar.setTitle("Main Title");
+        actionBar.setSubtitle("Subtitle");
+        setBackgroundColor();
+        setLogo();
+    }
+
+    private void customizeStatusBar()
+    {
+        if (android.os.Build.VERSION.SDK_INT >= 21)
+        {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(darkViolet);
+        }
     }
 
     private void setBackgroundColor()
     {
-        actionBar.setBackgroundDrawable( new ColorDrawable( violet ));
+        actionBar.setBackgroundDrawable( new ColorDrawable(violet) );
     }
 
     private void setLogo()
